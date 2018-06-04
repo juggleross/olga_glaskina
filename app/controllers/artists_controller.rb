952106d05fceb54037534bb52b2282f0c447ea3e
Vladislav Hilko <Hilko@fidor.com>
2018-06-04 12:20:40 +0300

    Was added rspec and pry

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,6 +2,7 @@ class ArtistsController < ApplicationController
   def show
     @artworks = Project.where(global_name: params[:id])
   end
+
   def index
     @artists = Artist.order(:short_name)
   end
